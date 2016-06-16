98684b6246f94310abec4a8e14b9bea6bc63062e
Eric Krause and Zach Holz <zach.a.holz@gmail.com>
2016-06-15 20:18:11 -0700

    Define global array variable for user input

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,17 @@ $(document).ready(function() {
   $("form").submit(function(event) {
 
     var foods = ["food1", "food2", "food3"];
+    var userInput = [];
 
     foods.forEach(function(food) {
-      var userInput = $("input#" + food).val();
-      $("ul#grocery-list").append(userInput);
-      });
+      userInput.push($("input#" + food).val());
 
+      // userInput.sort();
+      // var userList = userInput.toString();
+      // userList.toUpperCase();
+      // $("ul#grocery-list").append(userInput);
+      });
+    alert(userInput);
     event.preventDefault();
 
   });
